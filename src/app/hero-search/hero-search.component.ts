8312dc3d4ce4f36fdb912406bcecf5a4175eff28
peter_chao <peter_chao@dongfeng.com.tw>
2023-09-22 15:21:56 +0800

    feat:[Angular練習] 補充註解

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -22,10 +22,11 @@ export class HeroSearchComponent implements OnInit {
 
   ngOnInit(): void {
     this.heroes$ = this.searchTerms.pipe(
+      // 待字串變化的事件停止150毫秒後，才會發送請求
       debounceTime(150),
-      // 用於確保搜尋條件變化才會傳送請求給伺服器
+      // 用於確保搜尋條件變化(也就是搜尋輸入框內的文字有變化)才會傳送請求給伺服器
       distinctUntilChanged(),
-      // 當作為搜尋條件的字串有進行變化後，將調用搜尋服務
+      // 從經過上兩項觸發的請求中，只返回最近一次將其餘請求取消及捨棄，避免海量HTTP請求浪費伺服器資源
       switchMap((term: string) => this.heroService.searchHeroes(term)),
     );
   }
